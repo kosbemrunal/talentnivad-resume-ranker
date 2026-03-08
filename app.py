05c75104bc883a0c01302b540a3104fe71d9aba3
kosbemrunal <kosbemrunal@gmail.com>
2026-03-08 19:27:28 +0530

    Create database tables on startup

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///talentnivad.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
